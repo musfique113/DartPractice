d5a3ffbfefe18c71c4f12df60c204a6909588b2f
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-04-16 00:25:36 +0600

    tried multiple approaches

--- assignment01_module01/main/lib/main.dart
+++ assignment01_module01/main/lib/main.dart
@@ -12,16 +12,32 @@ void main() {
     "01768131685"
   ];
 
-  // phoneNumber[0] = phoneNumber[0].toString();
-  // phoneNumber[1] = phoneNumber[1].toString();
-  // phoneNumber[2] = phoneNumber[2].toString();
-  // phoneNumber[3] = phoneNumber[3].toString();
-  // phoneNumber[4] = phoneNumber[4].toString();
-
-  dynamic output = '${phoneNumber[0]}${phoneNumber[1]},${phoneNumber[0]}${phoneNumber[2]},${phoneNumber[0]}${phoneNumber[3]},${phoneNumber[0]}${phoneNumber[4]}';
+  dynamic output =
+      '${phoneNumber[0]}${phoneNumber[1]},${phoneNumber[0]}${phoneNumber[2]},${phoneNumber[0]}${phoneNumber[3]},${phoneNumber[0]}${phoneNumber[4]}';
   print("Full numbers: $output");
 
   //print out all numbers given in the list except +88
   List<dynamic> subList = phoneNumber.sublist(1, 5);
   print("Numbers given in the list except (+88): $subList");
+
+/* //another approches
+  String one = phoneNumber[0] + phoneNumber[1];
+  String two = phoneNumber[0] + phoneNumber[2];
+  String three = phoneNumber[0] + phoneNumber[3];
+  String four = phoneNumber[0] + phoneNumber[4];
+  print("$one,$two,$three,$four");
+
+  print(
+      '${phoneNumber[0]}${phoneNumber[1]},${phoneNumber[0]}${phoneNumber[2]},${phoneNumber[0]}${phoneNumber[3]},${phoneNumber[0]}${phoneNumber[4]}');
+
+  
+  phoneNumber[0] = phoneNumber[0].toString();
+  phoneNumber[1] = phoneNumber[1].toString();
+  phoneNumber[2] = phoneNumber[2].toString();
+  phoneNumber[3] = phoneNumber[3].toString();
+  phoneNumber[4] = phoneNumber[4].toString();
+
+ */
+
+  
 }
