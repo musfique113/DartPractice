e2bbafbd1e8c9b4565b5d1b307fd9d50c11ce490
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-04-17 00:47:19 +0600

    typeCasting

---
+++ dart_application_1/bin/operators.dart
@@ -0,0 +1,24 @@
+void main() {
+  var x = 68;
+  //var newS = x++;
+  print(++x);
+  print(x++);
+  //print(x);
+
+  const int yearIs = 30;
+  print(yearIs);
+
+  final int age;
+  age = 20;
+  print(age);
+
+  // const use , need to asign value is must in same line
+  // final use , value can asign later
+
+  //Type casting using Parse
+  String c = '330';
+  int l = int.parse(c); //string to intiger
+  print(l);
+  
+  // learn .toString
+}
