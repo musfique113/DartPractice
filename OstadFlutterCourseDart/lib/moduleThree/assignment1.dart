8a6346fde63a12ba2b407d7ad79a1dd75a73a134
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-05-14 17:04:38 +0600

    assignment module 3

--- OstadFlutterCourseDart/lib/moduleThree/assignment1.dart
+++ OstadFlutterCourseDart/lib/moduleThree/assignment1.dart
@@ -9,7 +9,8 @@ class Car {
 
   static int numberOfCars = 0;
 
-  Car(this.brand, this.model, this.year, this.milesDriven,this.age) {
+  //constructor 
+  Car(this.brand, this.model, this.year, this.milesDriven, this.age) {
     numberOfCars++;
     age = DateTime.now().year - year;
   }
@@ -18,6 +19,7 @@ class Car {
     milesDriven += miles;
   }
 
+  //getter method
   double getMilesDriven() => milesDriven;
 
   String getBrand() => brand;
@@ -35,10 +37,10 @@ void main() {
   Car car1 = Car("Toyota", "Corolla", 2022, 0, 10);
   car1.drive(100);
 
-  Car car2 = Car("Honda", "Civic", 2021, 0,12);
+  Car car2 = Car("Honda", "Civic", 2021, 0, 12);
   car2.drive(200);
 
-  Car car3 = Car("Nissan", "Altima", 2020, 0,13);
+  Car car3 = Car("Nissan", "Altima", 2020, 0, 13);
   car3.drive(300);
 
   print("Car 1");
@@ -47,6 +49,7 @@ void main() {
   print("Year: ${car1.getYear()}");
   print("Miles Driven: ${car1.getMilesDriven()}");
   print("Age: ${car1.getAge()}");
+  print("-----------------------------------------------");
 
   print("Car 2");
   print("Brand: ${car2.getBrand()}");
@@ -54,6 +57,7 @@ void main() {
   print("Year: ${car2.getYear()}");
   print("Miles Driven: ${car2.getMilesDriven()}");
   print("Age: ${car2.getAge()}");
+  print("-----------------------------------------------");
 
   print("Car 3");
   print("Brand: ${car3.getBrand()}");
@@ -61,6 +65,7 @@ void main() {
   print("Year: ${car3.getYear()}");
   print("Miles Driven: ${car3.getMilesDriven()}");
   print("Age: ${car3.getAge()}");
+  print("-----------------------------------------------");
 
   print("Total number of cars: ${Car.getNumberOfCars()}");
 }
