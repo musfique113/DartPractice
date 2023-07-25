99fd2283e0e5ad5b108bbad8a66ebf48a36ec09d
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-07-26 02:20:46 +0600

    user input test

--- test.dart
+++ test.dart
@@ -1,3 +1,33 @@
+// ?: Write a program to greet the user with their name.
+// *: learn about comments, imports, input, output in dart.
+
+// *: comments
+// single line comment
+/*  multi
+    line
+    comment */
+/// documentation comment, dartdoc treats these comments specially
+
+// *: imports
+// import 'dart:math';  // importing core libraries
+// import 'package:test/test.dart'; // importing libraries from external packages
+// import 'path/to/my_other_file.dart'; // importing files
+
+// *: annotations
+// int? a; // allows null values, removing '?' forces non-nullable
+
+import 'dart:io';
 void main() {
-  print("object");
+    stdout.write("Name: ");
+    String? name = stdin.readLineSync();
+
+    stdout.write("Age: ");
+    String? age = stdin.readLineSync();
+    //int? age = int.tryParse(ageString!);
+
+    stdout.write("Dept: ");
+    String? dept = stdin.readLineSync();
+
+    stdout.write("You are $name , $age years old from $dept depertment.\n");
+
 }
