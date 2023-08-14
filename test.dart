d7e5ac91aad6f34f79672a50e77a650f537263a7
musfique113 <musfique113@gmail.com>
2023-08-14 19:12:30 +0600

    d

--- test.dart
+++ test.dart
@@ -9,11 +9,6 @@
 /// documentation comment, dartdoc treats these comments specially
 
 // *: imports
-// import 'dart:math';  // importing core libraries
-// import 'package:test/test.dart'; // importing libraries from external packages
-// import 'path/to/my_other_file.dart'; // importing files
-
-// *: annotations
 // int? a; // allows null values, removing '?' forces non-nullable
 
 import 'dart:io';
